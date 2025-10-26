5564f92c454d37d5bd7cfefc2a49f8f35c0c2505
vanlinh00 <vanlinh20192019@gmail.com>
2025-10-26 07:44:26 +0700

    no message

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; // create empty index.css if missing
+import "./styles/index.css"; // create empty index.css if missing
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
